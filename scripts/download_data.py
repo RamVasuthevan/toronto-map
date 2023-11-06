486296d439437838538042cc9e42328479abe46a
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-11-06 16:48:07 +0000

    Checkpoint

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,48 +1,67 @@
+import os
 import json
 import requests
 import zipfile
+import shutil
 from io import BytesIO
-from typing import Dict
+from typing import Dict, List
+from urllib.parse import urlparse, unquote  # Import urlparse and unquote from urllib.parse
 from pprint import pprint
 
-LOBBY_ACTIVITY_FILE_NAME = "Lobbyist Registry Activity.zip"
-OPEN_DATA_RESPONSE_FILE_NAME = "open-data-response.json"
 API_BASE_URL = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
 API_PACKAGE_SHOW_URL = API_BASE_URL + "/api/3/action/package_show"
-API_PACKAGE_ID = "lobbyist-registry"
+API_PACKAGE_IDS = ["property-boundaries", "address-points-municipal-toronto-one-address-repository"]
+
+# Flag to decide whether to unzip the downloaded zip files
+UNZIP_FILES = False
 
 
-def get_package() -> Dict:
+def get_package(package_id: str) -> Dict:
     """Returns response from Toronto Open Data CKAN API"""
-
-    params = {"id": API_PACKAGE_ID}
+    params = {"id": package_id}
     package = requests.get(API_PACKAGE_SHOW_URL, params=params).json()
-
     return package
 
 
-def downdload_data():
-    package = get_package()
-    pprint(package)
+def extract_zip(resource: str, package_id: str):
+    """Extracts the contents of a zip file"""
+    with open(resource, "rb") as f:
+        zip_file = zipfile.ZipFile(BytesIO(f.read()))
+        zip_file.extractall(path=package_id)
 
-    with open(OPEN_DATA_RESPONSE_FILE_NAME, "w") as json_file:
-        json_file.write(json.dumps(package, indent=4))
 
-    resource_response = {}
-    for resource in package["result"]["resources"]:
-        resource_response[
-            f"{resource['name']}.{resource['format'].lower()}"
-        ] = requests.get(resource["url"])
+def download_data(package_ids: List[str], unzip_files: bool = True):
+    for package_id in package_ids:
+        # Delete the directory for the package if it exists
+        if os.path.exists(package_id):
+            shutil.rmtree(package_id)
+        
+        package = get_package(package_id)
+        pprint(package)
 
-    for resource, response in resource_response.items():
-        with open(resource, "wb") as f:
-            f.write(response.content)
+        # Create a directory for the package if it doesn't exist
+        if not os.path.exists(package_id):
+            os.makedirs(package_id)
 
-    lobbyactivity_zip = zipfile.ZipFile(
-        BytesIO(resource_response[LOBBY_ACTIVITY_FILE_NAME].content)
-    )
-    lobbyactivity_zip.extractall()
+        open_data_response_file_name = os.path.join(package_id, f"{package_id}-open-data-response.json")
+        with open(open_data_response_file_name, "w") as json_file:
+            json_file.write(json.dumps(package, indent=4))
+
+        resource_response = {}
+        for resource in package["result"]["resources"]:
+            # Extract the correct file name and extension from the URL
+            url_path = urlparse(unquote(resource['url'])).path
+            file_name = os.path.basename(url_path)
+            file_path = os.path.join(package_id, file_name)  # Save the resource in the package directory
+            resource_response[file_path] = requests.get(resource["url"])
+
+        for resource, response in resource_response.items():
+            with open(resource, "wb") as f:
+                f.write(response.content)
+
+            if unzip_files and resource.endswith(".zip"):
+                extract_zip(resource, package_id)  # Extract the zip file contents into the package directory
 
 
 if __name__ == "__main__":
-    downdload_data()
+    download_data(API_PACKAGE_IDS, unzip_files=False)
